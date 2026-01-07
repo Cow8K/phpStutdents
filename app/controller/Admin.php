867868a8298aff8b8403ea76b5e374a2e002467b
Liu Sheng <894349920@qq.com>
2026-01-07 17:08:19 +0800

    prof: 使用 thinkphp 模板继承优化代码

--- app/controller/Admin.php
+++ app/controller/Admin.php
@@ -3,11 +3,31 @@
 namespace app\controller;
 
 use app\BaseController;
+use think\App;
 use think\facade\Db;
+use think\facade\View;
+use think\facade\Request;
 use think\facade\Session;
 
 class Admin extends BaseController
 {
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+
+        $menus = [
+            "/admin/index" => "用户管理",
+            "/admin/adminManage" => "管理员管理"
+        ];
+
+        $actionName = '/' . Request::controller(true) . '/' . Request::action();
+
+        View::assign([
+            "menus" => $menus,
+            "actionName" => $actionName
+        ]);
+    }
+
     public function index()
     {
         $userInfo = Session::get('userInfo');
@@ -17,7 +37,7 @@ class Admin extends BaseController
 
         $pageData = [
             'userInfo' => $userInfo,
-            'active' => 1
+            'title' => '用户中心'
         ];
 
         return view('user_center', $pageData);
@@ -28,8 +48,8 @@ class Admin extends BaseController
         $adminGroups = Db::table('admin_group')->where('name', '!=', '超级管理员')->select();
 
         $pageData = [
-            'active' => 2,
-            'adminGroups' => $adminGroups
+            'adminGroups' => $adminGroups,
+            'title' => '管理员管理'
         ];
 
         return view('admin_manage', $pageData);
